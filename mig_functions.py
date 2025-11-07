558256d3692b56bd613cd881c35f683f799f6c67
jeremyparkin <djplana@gmail.com>
2025-11-06 22:59:21 -0500

    FAST grouping

--- mig_functions.py
+++ mig_functions.py
@@ -2,7 +2,7 @@ def standard_sidebar():
     import streamlit as st
     st.sidebar.image('https://app.agilitypr.com/app/assets/images/agility-logo-vertical.png', width=180)
     st.sidebar.subheader('MIG Bulk AI Analysis App')
-    st.sidebar.caption("Version: July 2025")
+    st.sidebar.caption("Version: Nov 2025")
 
     # CSS to adjust sidebar
     adjust_nav = """
